46e879e14df0b3afe1e98f97cf4098c1f27befa6
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-03-20 21:06:58 +0600

    React Project

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-        <h3>Devoloper is Sleeping </h3>
+            <h3>Devoloper is Sleeping </h3>
         </div>
     );
 };
